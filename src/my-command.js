88337efe0077fb8516879fd11ceee33989b0cb29
James Ferguson <james.ferguson@skyscanner.net>
2017-12-20 09:56:46 +0000

    Initial commit

--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,34 @@
-export default function(context) {
-  context.document.showMessage("It's alive 🙌")
-}
+export default function checkFontSize() {
+  var sketch = context.api();
+
+  if (context.selection.length === 0) {
+    sketch.selectedDocument.showMessage("No selection" );
+  } else {
+    fontSize(sketch.selectedDocument.selectedLayers);
+  }
+};
+
+function fontSize(selection) {
+  // declare permitted fontsizes
+  var permittedFontSizes = [12,16,24,28,42];
+
+  var anyViolation = false;
+
+  selection.iterate((layer) => {
+    if (layer.isText) {
+      var permittedSize = permittedFontSizes.indexOf(layer.sketchObject.fontSize()) != -1;
+
+      if (!permittedSize) {
+        anyViolation = true;
+        layer.name = `*${layer.name}`
+      } else {
+        layer.deselect();
+      }
+    }
+  });
+
+  if (anyViolation) {
+    context.document.showMessage("Uh oh. Wrong font sizes detected.");
+
+  }
+};
